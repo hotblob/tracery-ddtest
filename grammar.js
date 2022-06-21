1219d2f8b6a2b4eca2c153721d53a99d25ffdc48
hotblob <104743968+hotblob@users.noreply.github.com>
2022-06-20 18:36:58 -0700

    first draft

---
+++ grammar.js
@@ -0,0 +1,22 @@
+var grammar = {
+	"origin":["<p>#name#, the #demeanor# #profession#.</p>"],
+	
+	"name":[
+		"Terrance",
+		"Tobin",
+		"Drathaniel",
+		"Herrywinkle"],
+
+	"demeanor":[
+		"jovial",
+		"morose",
+		"staunchy",
+		"tattlemongering"],
+
+	"profession":[
+		"janitor",
+		"postman",
+		"baker",
+		"schoolmaster"
+	],
+}
